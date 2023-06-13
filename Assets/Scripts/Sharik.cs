8b5fe469fbf5e4b3797f4b64342140a8a0def0e6
Rulett <solomkin.leonid.a@gmail.com>
2023-06-13 17:04:56 +0300

    greh

--- Assets/Scripts/Sharik.cs
+++ Assets/Scripts/Sharik.cs
@@ -1,34 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Sharik : MonoBehaviour
 {     float horizontalInput;
       public float speed;
       public float jumpHeight;
       int kadri;
+      protected Rigidbody2D shar;
+      private bool yaNaZemle;
       
       // Start is called before the first frame update
     void Start()
     {
-        
+        shar = GetComponent<Rigidbody2D>();
+        yaNaZemle=false;
     }
 
     // Update is called once per frame
-    void LateUpdate()
-    {
+    void Update()
+    {     
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime *speed*horizontalInput);
-        
-       // if (Input.GetAxis("Jump")!=0) kadri=(int)(1/Time.deltaTime);
-      //  if (kadri!=0) {transform.Translate(Vector3.up * Time.deltaTime *jumpHeight); kadri--;}
+        //transform.Translate(Vector3.right * Time.deltaTime *speed*horizontalInput);
+        if (Math.Abs(shar.velocity.x)<10) shar.AddForce(Vector2.right * Time.deltaTime *speed*horizontalInput*100);
 
 
-         if (Input.GetAxis("Jump")!=0) transform.Translate(Vector3.up * Time.deltaTime *jumpHeight);
-        
 
-
-        
+        if (Input.GetKeyDown(KeyCode.Space) && yaNaZemle )
+        {
+            shar.AddForce(new Vector2(0,jumpHeight), ForceMode2D.Impulse);
+            yaNaZemle=false;
+            
+        }
 
     }
+
+
+    void OnCollisionEnter2D(Collision2D predmet)
+    {
+
+          if (predmet.gameObject.tag == "Zemlya") yaNaZemle=true;
+
+
+
+    }
+
 }
