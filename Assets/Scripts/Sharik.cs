09745b412ec7e6f062b1bca8a617795c3aa6f4a7
Rulett <solomkin.leonid.a@gmail.com>
2023-06-22 20:28:35 +0300

    granata upgrade

--- Assets/Scripts/Sharik.cs
+++ Assets/Scripts/Sharik.cs
@@ -10,18 +10,27 @@ public class Sharik : MonoBehaviour
       protected Rigidbody2D shar;
       private bool yaNaZemle;
       private bool yaNaStene;
-      public float timer=1; 
-      public bool play=true; 
+      private float timer=1; 
+      private bool play=true; 
       private int wallJumped=0;
       public GameObject granata;
+      private DateTime metka;
+      public double cooldown=1;
+      private DateTime cd;
       
       
       // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = timer;
+
+
         shar = GetComponent<Rigidbody2D>();
         yaNaZemle=false;
+
+
+        cd=new DateTime().AddSeconds(cooldown);
+        
     }
 
     // Update is called once per frame
@@ -30,16 +39,22 @@ public class Sharik : MonoBehaviour
         shar.AddForce(Vector2.right * Time.deltaTime *speed*Input.GetAxis("Horizontal")*100);
         shar.AddForce(new Vector2((float)Math.Pow((shar.velocity.x),2)*(shar.velocity.x<0?1:-1),0)*Time.deltaTime*10);
 
+
         if (Input.GetKeyDown(KeyCode.Space)) JumpAttempt();
 
-
-        if (Input.GetKeyDown(KeyCode.G)) BrosokGranati();
+        
+        if ((Input.GetKeyDown(KeyCode.G)) && (DateTime.Now.Subtract(metka)>cd.Subtract(new DateTime()))) BrosokGranati();
 
 
         
         if (Input.GetKeyDown(KeyCode.Escape) && play) {timer=0; play=false;}
         else if (Input.GetKeyDown(KeyCode.Escape) && !play) {timer=1; play=true;}
         Time.timeScale = timer;
+
+
+
+
+
     }
 
     public void NaZemle(bool pravda)
@@ -66,11 +81,29 @@ public class Sharik : MonoBehaviour
 
 
     void BrosokGranati()
-    {
-        Instantiate(granata, transform).GetComponent<Rigidbody2D>().AddForce(new Vector2(100,0), ForceMode2D.Impulse);
-
+    { 
+        float direction = (float)Math.Sqrt(Math.Pow(shar.velocity.x,2)+Math.Pow(shar.velocity.y,2))/10;
+        Instantiate(granata, transform).GetComponent<Rigidbody2D>().AddForce(new Vector2(shar.velocity.x/direction,shar.velocity.y/direction), ForceMode2D.Impulse);
+        metka = DateTime.Now;
 
     }
 
-
+     void OnGUI()
+    {
+        TimeSpan counter=cd.Subtract(new DateTime()).Subtract(DateTime.Now.Subtract(metka));
+       
+        int w = Screen.width, h = Screen.height;
+ 
+        GUIStyle style = new GUIStyle();
+ 
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 5 / 100; 
+        style.normal.textColor = Color.black;
+        string text=counter.Seconds.ToString()+"."+((int)((double)(counter.Milliseconds)/100)).ToString();
+        if (DateTime.Now.Subtract(metka)<cd.Subtract(new DateTime())) GUI.Label(rect, text, style);
+       
+       
+    }
+ 
 }
